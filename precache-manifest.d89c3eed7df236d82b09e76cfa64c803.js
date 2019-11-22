71c11cc00df2e56161d12e7ff7c26feeff6d6718
Xavier <x_hofmann11@cs.uni-kl.de>
2019-11-22 23:01:33 +0100

    Updates

---
+++ precache-manifest.d89c3eed7df236d82b09e76cfa64c803.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147c723facc947c7d13c2eb55d0e54a1",
+    "url": "/ching-chong/index.html"
+  },
+  {
+    "revision": "dc2f897b7c096d85176a",
+    "url": "/ching-chong/static/css/main.3188b1fb.chunk.css"
+  },
+  {
+    "revision": "6bea2a1750d5e71fa6f7",
+    "url": "/ching-chong/static/js/2.91120321.chunk.js"
+  },
+  {
+    "revision": "dc2f897b7c096d85176a",
+    "url": "/ching-chong/static/js/main.464c6bae.chunk.js"
+  },
+  {
+    "revision": "da074830d0c0e2380596",
+    "url": "/ching-chong/static/js/runtime~main.8123b7ad.js"
+  }
+]);
