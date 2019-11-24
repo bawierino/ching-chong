044db314093775ea6306f9934ee949309f71cba0
Xavier <x_hofmann11@cs.uni-kl.de>
2019-11-24 14:58:43 +0100

    fix routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import './App.css';
 import { FilterPageComponent } from './components/filter_page_component/filter_page_component';
 import { words } from './data/words';
+import { useRouterInitialization } from './hooks/use_router_initialization';
 
 export function App() {
+	useRouterInitialization();
+
 	return <div className="app">{FilterPageComponent(Object.values(words).map((value) => value))}</div>;
 }
--- src/services/router_service.ts
+++ src/services/router_service.ts
@@ -1,6 +1,14 @@
 class RouterService {
+	private readonly firstPathParameter: string = 'ching-chong';
+
+	public initialize(): void {
+		if (!document.location.pathname.startsWith(`/${this.firstPathParameter}`)) {
+			window.history.replaceState(undefined, '', `/${this.firstPathParameter}`);
+		}
+	}
+
 	public navigateToPath(path: string): void {
-		window.history.pushState(undefined, '', path);
+		window.history.pushState(undefined, '', `/${this.firstPathParameter}/${path}`);
 	}
 
 	public navigateBack(): void {
@@ -8,12 +16,12 @@ class RouterService {
 	}
 
 	public hasPath(): boolean {
-		return document.location.pathname.length > 1;
+		return document.location.pathname.replace(`/${this.firstPathParameter}`, '').length > 1;
 	}
 
 	public getPath(): string {
 		if (this.hasPath()) {
-			return document.location.pathname.split('/')[1];
+			return document.location.pathname.split('/')[2];
 		}
 
 		return undefined;
