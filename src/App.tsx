31700d2b0513d69542b684811c60a8fda0e2dc75
Xavier <x_hofmann11@cs.uni-kl.de>
2019-11-25 21:24:31 +0100

    sort filter page cards by pinyin

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import './App.css';
 import { FilterPageComponent } from './components/filter_page_component/filter_page_component';
 import { words } from './data/words';
 import { useRouterInitialization } from './hooks/use_router_initialization';
+import { Word } from './data/structures/word';
 
 export function App() {
 	useRouterInitialization();
 
-	return <div className="app">{FilterPageComponent(Object.values(words).map((value) => value))}</div>;
+	return (
+		<div className="app">
+			{FilterPageComponent(
+				Object.values(words).map((value) => value).sort((a, b) => {
+					function getPinyin(word: Word): string {
+						return word.characters.map((w) => w.pinyin).join('');
+					}
+
+					return getPinyin(a).localeCompare(getPinyin(b));
+				})
+			)}
+		</div>
+	);
 }
