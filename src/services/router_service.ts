66edc89d02814ea24a27593c7ce6be90e6054366
Xavier <x_hofmann11@cs.uni-kl.de>
2019-12-03 22:41:36 +0100

    initialize router so that if the url which the page is accessed with contains a hash the back page is set to the overview page

--- src/services/router_service.ts
+++ src/services/router_service.ts
@@ -2,13 +2,21 @@ class RouterService {
 	private readonly firstPathParameter: string = 'ezpzchinez';
 
 	public initialize(): void {
+		const { hash } = document.location;
 		if (!document.location.pathname.startsWith(`/${this.firstPathParameter}`)) {
-			window.history.replaceState(undefined, '', `/${this.firstPathParameter}`);
+			window.history.replaceState(undefined, '', `/${this.firstPathParameter}${hash}`);
+		}
+		if (performance.navigation.type !== 1 && this.hasHash()) {
+			// not reloading page and the page has a hash
+			// use case: send specific word to focus via url
+			// set the back state to the overview page
+			window.history.pushState(undefined, '', `/${this.firstPathParameter}`);
+			this.navigateToHash(hash.replace('#', ''));
 		}
 	}
 
-	public navigateToHash(path: string): void {
-		window.history.pushState(undefined, '', `/${this.firstPathParameter}#${path}`);
+	public navigateToHash(hash: string): void {
+		window.history.pushState(undefined, '', `/${this.firstPathParameter}#${hash}`);
 		window.dispatchEvent(new Event('popstate'));
 	}
 
